bbe1a86810b0316ab45037f1fa9e89e792625e72
Lyl55 <88458345+Lyl55@users.noreply.github.com>
2023-02-20 21:24:08 +0400

    first commit

---
+++ BlazorServerWebUI/Models/OrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorServerWebUI.Models
+{
+    public class OrderViewModel
+    {
+        public string OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderedOn { get; set; }
+        public List<OrderDetailViewModel> OrderDetails { get; set; }
+    }
+
+    public class OrderDetailViewModel
+    {
+        public int ProductId { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ BlazorServerWebUI/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Net.Http;
+using BlazorServerWebUI.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://apigateway") });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
